3e96f6b19697747129b5440478189b50c523b7d6
Monster Techno <officialworksuman@gmail.com>
2021-03-19 23:51:13 +0530

    Initial commit

---
+++ app/src/main/java/com/monstertechno/moderndashbord/MainActivity.java
@@ -0,0 +1,21 @@
+package com.monstertechno.moderndashbord;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(1);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
